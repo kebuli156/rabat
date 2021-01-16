75e5e5b13984d766b0987927b9133cd23c9fa99f
Faris Maulana Nasution <electricladyland156@gmail.com>
2021-01-16 07:08:55 +0700

    Add http client

---
+++ src/rabat/components/http_client.clj
@@ -0,0 +1,26 @@
+(ns rabat.components.http-client
+  (:require
+   [clj-http.client :as http-clt]))
+
+(defrecord HttpClient [])
+
+(defn http-client
+  ([config]
+   (http-client config {}))
+  ([config opts]
+   (map->HttpClient (assoc opts :config config))))
+
+(defn request
+  [{:keys [config]} http-method uri req]
+  (let [caller  (case http-method
+                  :get     http-clt/get
+                  :head    http-clt/head
+                  :post    http-clt/post
+                  :put     http-clt/put
+                  :delete  http-clt/delete
+                  :patch   http-clt/patch
+                  :options http-clt/options
+                  :copy    http-clt/copy
+                  :move    http-clt/move)
+        new-uri (str (:host-uri config) uri)]
+    (caller new-uri req)))
